6a394082d161e4e3fa364ca36d4cd28970f75cc0
David <hamiltondm@merrimack.edu>
2025-10-28 01:21:32 -0400

    small tweaks and updates on files

--- src/ast/EvaluationException.java
+++ src/ast/EvaluationException.java
@@ -1,37 +1,27 @@
 /*
- *   Copyright (C) 2022 -- 2025  Zachary A. Kissel
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  This is the EvaluationException class.
+ *  This is used to represent any evaluation problem in the interpreter.
+ *  This is thrown whenever something goes wrong during program execution,
+ *  like unbound variables, invalid types, or bad operations.
+ *  Author: David Hamilton
  */
+
 package ast;
 
-/**
- * An exception that represents an evaluation problem.
- * 
- * @author Zach Kissel
- */
 public class EvaluationException extends Exception {
-    /**
-     * The constructor for an evaluation exception that
-     * prints the line number and a statement of failure.
-     */
+
+    // This is the constructor with a default message
     public EvaluationException() {
         super("Interpretation failed.");
     }
 
-    // Add this constructor to define EvaluationException(String)
+    // This is the constructor that takes a custom message
     public EvaluationException(String message) {
         super(message);
+    }
+
+    // This is the constructor that lets you pass a cause (optional)
+    public EvaluationException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
