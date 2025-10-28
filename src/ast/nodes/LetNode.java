6bb6d20776db5433adf10ed25eb2ed8c6c13c837
David <hamiltondm@merrimack.edu>
2025-10-28 01:18:12 -0400

    updated letnode

--- src/ast/nodes/LetNode.java
+++ src/ast/nodes/LetNode.java
@@ -1,77 +1,58 @@
 /*
- *   Copyright (C) 2022 -- 2025  Zachary A. Kissel
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  This is the LetNode class.
+ *  This is responsible for evaluating: let <id> := <expr> in <expr>
+ *  This is where we create a new scope, bind the name, and evaluate the body.
+ *  Author: David Hamilton
  */
+
 package ast.nodes;
 
 import ast.EvaluationException;
 import environment.Environment;
 import lexer.Token;
 
-/**
- * let <id> := <expr> in <expr>
- */
-public final class LetNode extends SyntaxNode
-{
-    private final Token name;            // identifier token (holds the lexeme)
-    private final SyntaxNode valueExpr;  // E1
-    private final SyntaxNode body;       // E2
+public final class LetNode extends SyntaxNode {
 
-    public LetNode(Token name, SyntaxNode valueExpr, SyntaxNode body, long lineNumber)
-    {
-        super(lineNumber);
-        this.name = name;
-        this.valueExpr = valueExpr;
+    // This is the identifier being introduced by the let
+    private final Token id;
+
+    // This is the expression that computes the value to bind to the identifier
+    private final SyntaxNode boundExpr;
+
+    // This is the body expression that runs with the new binding in scope
+    private final SyntaxNode body;
+
+    // This is the constructor wiring up id, bound expression, body, and line number
+    public LetNode(Token id, SyntaxNode boundExpr, SyntaxNode body, long line) {
+        super(line);
+        this.id = id;
+        this.boundExpr = boundExpr;
         this.body = body;
     }
 
-    /**
-     * ⟦let I := E1 in E2⟧_e  =  ⟦E2⟧_{ copy(e)[ I ← ⟦E1⟧_e ] }
-     */
+    // This is for AST visualization (used by --ast)
     @Override
-    public Object evaluate(Environment env) throws EvaluationException
-    {
-        try {
-            // 1) Evaluate the bound expression in the ORIGINAL env
-            Object boundValue = valueExpr.evaluate(env);
-
-            // 2) Create an inner environment and bind I -> value
-            Environment inner = env.copy();
-            inner.updateEnvironment(name, boundValue);
-
-            // 3) Evaluate the body in the extended environment
-            return body.evaluate(inner);
-        } catch (EvaluationException e) {
-            // Already a user-facing error; include line number
-            logError(e.getMessage());
-            throw e;
-        } catch (Exception e) {
-            String msg = buildErrorMessage("Error evaluating let: " + e.getMessage());
-            logError(msg);
-            throw new EvaluationException();
-        }
+    public void displaySubtree(int indentAmt) {
+        printIndented("Let(" + id.getValue() + " := ... in ...)", indentAmt);
+        printIndented("This is the bound expression:", indentAmt + 1);
+        boundExpr.displaySubtree(indentAmt + 2);
+        printIndented("This is the body:", indentAmt + 1);
+        body.displaySubtree(indentAmt + 2);
     }
 
+    // This is the evaluate method that enforces lexical scoping
     @Override
-    public void displaySubtree(int indentAmt)
-    {
-        printIndented("Let", indentAmt);
-        printIndented("name: " + name.getValue(), indentAmt + 2);
-        printIndented("value:", indentAmt + 2);
-        valueExpr.displaySubtree(indentAmt + 4);
-        printIndented("in:", indentAmt + 2);
-        body.displaySubtree(indentAmt + 4);
+    public Object evaluate(Environment env) throws EvaluationException {
+        // This is evaluating the bound expression in the current environment
+        Object value = boundExpr.evaluate(env);
+
+        // This is making a new environment (copy) for the body so we get proper shadowing
+        Environment child = env.copy();
+
+        // This is binding the identifier in the new environment
+        child.updateEnvironment(id.getValue(), value);
+
+        // This is evaluating the body in the new (child) environment
+        return body.evaluate(child);
     }
 }
